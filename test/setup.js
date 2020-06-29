8c7f342a5e9cf7df44347d5a30fa7500bbf2f916
Kevin Burdett <kevin.burdett@pps.io>
2020-06-29 16:54:38 -0400

    Update dependencies and modernize project setup

--- test/setup.js
+++ test/setup.js
@@ -5,8 +5,3 @@ const sinonChai = require('sinon-chai');
 global.should = chai.should();
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
-
-global.expect = chai.expect;
-global.AssertionError = chai.AssertionError;
-global.Assertion = chai.Assertion;
-global.assert = chai.assert;
