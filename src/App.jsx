facef2e07b2b75ddb5c96305b0f90840f458e045
Sostenes Apollo <ctr.sostenes.dasilva@prth.com>
2024-02-03 18:31:34 -0300

    Add Cv link and update image

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { loadFull } from "tsparticles";
 const experiences= [
   {
     title:'Priority Payment Systems - BairesDev',
-    year: '2021 - Current',
+    year: '2021 - 2024',
     location: 'Alphareta, GA, US',
     description: `
     I was responsible for the development of payment microservices using node.js and TDD practices, with a focus on delivering high-quality, scalable solutions. My technical skills in Javascript, Typescript, and C# enabled me to effectively develop and maintain these microservices, utilizing Chai and Jest for test-driven development. My contributions significantly improved the reliability and performance of the payment microservices, resulting in a 25% increase in transaction processing speed. I also played a key role in the maintenance of Kafka, Redis, streamliners, and APIs for these microservices.
@@ -106,11 +106,21 @@ const moreTools = [
 ]
 
 const MeIcon = ({className="", onClick}) => <button onClick={onClick} type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className={`inline-block p-1 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out ${className}`} style={{backgroundColor:'#333'}}>
-  <img src="me.jpeg" alt="ME" className={`inline-block text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-12 sm:w-20 ${className}`}/>
+  <img src="sostenes.jpeg" alt="ME" className={`inline-block text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-12 sm:w-20 ${className}`}/>
 </button>
 
 const GithubIcon = ({className=""}) => <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className={`inline-block p-2 mb- text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out ${className}`} style={{backgroundColor:'#333'}}>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="w-7 h-7"><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
+</button>
+
+const ResumeIcon = ({className=""}) => <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className={`flex inline-block p-2 mb- text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out ${className}`} style={{backgroundColor:'#333'}}>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+</svg>
+<div className="pt-1 pl-2">
+  Donwload CV
+</div>
+
 </button>
 
 const TwitterIcon= ({className=""}) => <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className={`inline-block p-2 mb-2 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out ${className}`} style={{backgroundColor:'#1da1f2'}}>
@@ -154,6 +164,9 @@ const Title = () => {
     <div className="flex">
       {/* <h2 id="industry-title" class="print:text-6xl text-center lg:text-xl md:text-md mr-auto text-md font-semibold text-gray-750 pb-px"> */}
         {/* Software Engineer */}
+        <a  className="w-auto" href="https://docs.google.com/document/d/1qUf3PMYQoJNL-5P1Pt3HmB7L-P7ebl0Efs_O79bT874/edit?usp=sharing" target="_blank" rel="noreferrer">
+          <ResumeIcon className="mr-2"/>
+        </a>
         <a  className="w-auto" href="https://github.com/sostenesapollo" target="_blank" rel="noreferrer">
           <GithubIcon className="mr-2"/>
         </a>
@@ -212,7 +225,7 @@ const Experience = ({
         {usefullLinks.length > 0 && <b className="pl-3">Usefull links/refs:</b>}
         <p></p>
         {usefullLinks.map((e)=>
-          <li className="pl-7 text-blue-300">
+          <li className="text-blue-300 pl-7">
             <a href={e.url} target="_blank" rel="noreferrer" className="text-blue-300">
               <RightArrow className="pl-3"/> {e.name}
             </a>
@@ -424,17 +437,7 @@ export default function App() {
             }}
         />
         
-        <div className="
-          p-10
-          mx-auto
-          print:max-w-letter
-          md:max-w-letter md:h-letter
-          xsm:p-8
-          sm:p-9
-          md:p-20
-          md:ml-20
-          md:mr-20
-        ">
+        <div className="p-10 mx-auto print:max-w-letter md:max-w-letter md:h-letter xsm:p-8 sm:p-9 md:p-20 md:ml-20 md:mr-20">
           <Title/>
           <div class="grid lg:grid-cols-4 gap-20">
             <ContentHome/>
